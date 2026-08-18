9cd36d5b3122e143f12789b8b276f9812474307c
Shivansh Mishra <Thebrownhuman@gmail.com>
2026-08-18 13:41:50 +0530

    fix(test): use default supertest import (namespace import breaks under current typings)

--- backend/test/auth.e2e-spec.ts
+++ backend/test/auth.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import * as request from 'supertest';
+import request from 'supertest';
 import { Pool } from 'pg';
 import * as bcrypt from 'bcrypt';
 import { AppModule } from '../src/app.module';
